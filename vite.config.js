371255210b01b19d13bf4496c59d00c8fe93ce98
xchnly <hendryhuang70@gmail.com>
2025-03-29 08:33:14 +0700

    Gaad berubah

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // Tambahkan import path
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src') // Tambahkan alias @ untuk src/
+      '@': path.resolve(__dirname, 'src')
     },
   },
+  server: {
+    host: '0.0.0.0', // Izinkan akses dari jaringan
+    port: 5173, // Bisa diubah jika perlu
+    strictPort: true, // Pastikan menggunakan port ini
+    open: true, // Otomatis buka di browser
+  }
 })
